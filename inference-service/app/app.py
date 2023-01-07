2bc538ed30bc068b9fd05d71ca57d8a71f7d5a9f
Andres Becker <abecker.esfm@gmail.com>
2023-01-07 21:35:53 +0100

    kafka event adapted to comply with the CloudEvents 1 specification

--- inference-service/app/app.py
+++ inference-service/app/app.py
@@ -4,11 +4,54 @@ from bson.objectid import ObjectId
 import pymongo
 import yaml
 import kafka
+from datetime import datetime as dt
+import sys
 
 class KafkaHandler(logging.Handler):
+    """
+    This logging.Handler sub-class allows the logging instance to send logs to kafka
+    """
     def emit(self, record):
         producer.send(kafka_topic, self.format(record))
         producer.flush()
+
+def _create_kafka_event(request_rc=200, job_id='null'):
+    """
+    Create an event on kafka that complies with the CloudEvents 1.0 specification.
+    This function collects information about the function corresponding to an 
+    operationId defined on the OpenAPI specification. Do not call this function 
+    unless it is done from a function corresponding to operationId on the OpenAPI 
+    specification. 
+    input:
+        request_rc: int, http request return code
+        job_id: str, job id if applicable, otherwise 'null'
+    output: None 
+    """
+    op_name = 'app.' + sys._getframe(1).f_code.co_name
+
+    op_info = None
+    for path in api_def['paths'].keys():
+        for r_meth in api_def['paths'][path].keys():
+            if op_name == api_def['paths'][path][r_meth]['operationId']:
+                op_info = {
+                    'operationId': op_name, 
+                    'path': path, 
+                    'request_method': r_meth, 
+                    'request_rc': request_rc,
+                    'job_id': job_id}
+
+    if op_info is None:
+        raise Exception(f'operationId not {op_name} found in the OpenAPI definition!')
+
+    event = {
+        'specversion' : "1.0",
+        'time': dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+    
+    event.update(op_info)
+
+    logger.info('%s', event)
+
 
 def _find_job(id: str):
     """
@@ -28,7 +71,7 @@ def _find_job(id: str):
 
 def list_inference_jobs():
 
-    logger.debug("Inference jobs listed")
+    #logger.debug("Inference jobs listed")
 
     # Retrieve all documents from the jobs collection
     # TODO: do something to avoid dumping the whole collection without affecting the functionality
@@ -39,6 +82,9 @@ def list_inference_jobs():
     for job in job_collec:
         job['id'] = str(job['_id'])
         jobs_list.append({key: job[key] for key in job_prop.keys() if key in job.keys()})
+
+    # Create kafka event
+    _create_kafka_event()
 
     # Return only the first thousand jobs
     return jobs_list, 200
@@ -60,7 +106,8 @@ def create_inference_job():
     # get job id and store it in the dic as str
     req_json['id'] = str(req_json['_id'])
 
-    logger.info(f"Inference job created with id: {req_json['id']}")
+    # Create kafka event
+    _create_kafka_event(201, req_json['id'])
 
     # Return only the properties defined on the api job schema
     return {key: req_json[key] for key in job_prop.keys() if key in req_json.keys()}, 201
@@ -72,13 +119,15 @@ def get_inference_job(id: str):
 
     # Return 404 if job could not be found
     if job is None:
-        logger.warning(f"Inference job with id: {id} not found")
+        # Create kafka event
+        _create_kafka_event(404, id)
         return {}, 404
-
-    logger.debug(f"Inference job with id: {id} information retrieved")
 
     # Add/rename job id
     job['id'] = str(job['_id'])
+
+    # Create kafka event
+    _create_kafka_event(200, id)
 
     # Return only the properties defined on the api job schema
     return  {key: job[key] for key in job_prop.keys() if key in job.keys()}, 200
@@ -90,13 +139,15 @@ def delete_inference_job(id: str):
 
     # Return 404 if job could not be found
     if job is None:
-        logger.warning(f"Inference job with id: {id} requested to be deleted, but id not found")
+        # Create kafka event
+        _create_kafka_event(404, id)
         return {}, 404
 
     # Remove job from collection
     jobs.delete_one({'_id': ObjectId(id)})
     
-    logger.info(f"Inference job with id: {id} deleted")
+    # Create kafka event
+    _create_kafka_event(204, id)
 
     return '', 204
     
@@ -121,8 +172,8 @@ logger.setLevel(logging.DEBUG)
 # Set custom Kafka logging handler
 handler = KafkaHandler()
 # Set the logging format
-formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s','%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
+#formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s','%Y-%m-%d %H:%M:%S')
+#handler.setFormatter(formatter)
 # Add the handler to the logger
 logger.addHandler(handler)
 
