aba6c9b5170ef02379453508f87da5f985afbd1e
Andres Becker <abecker.esfm@gmail.com>
2023-01-09 01:56:20 +0100

    almost working, base 64 image needs to be treated as binary and not as a string

--- inference_models/kafka_consumer.py
+++ inference_models/kafka_consumer.py
@@ -1,5 +1,28 @@
 from kafka import KafkaConsumer
 import json
+from PIL import Image
+import base64
+from io import BytesIO
+import tensorflow as tf
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+
+def _prepare_image(encoded_img, img_size=(299,299)):
+    print(len(encoded_img), encoded_img[0:10], encoded_img[-10:])
+    # Decode image
+    decoded_img = base64.b64decode(encoded_img)
+    print(len(decoded_img), decoded_img[0:10], encoded_img[-10:])
+    # Convert image into an array
+    img = Image.open(BytesIO(decoded_img))
+    img = img.convert('RGB')
+    img = np.asarray(img)
+    # Add batch dimension
+    img = np.expand_dims(img, axis=0)
+    # Resize and preprocess image
+    img = tf.image.resize(img, img_size)
+    img = preprocess_input(img)
+    
+    # return image as numpy array
+    return img.numpy()
 
 consumer = KafkaConsumer(
     #bootstrap_servers='kafka:9092',
@@ -14,4 +37,12 @@ consumer.subscribe(['infer_serv'])
 
 for message in consumer:
     # process the message here
-    print(message.value.decode('utf-8'))
+    kafka_msg = message.value.decode('utf-8')
+    kafka_msg = json.loads(kafka_msg)
+    print(kafka_msg)
+    if 'img' in kafka_msg.keys():
+        img = kafka_msg['img']
+        img = _prepare_image(img)
+        print(img.shape)
+
+    # Issue: image in base 64 encoding is read as string and not as binary, which is causing the issue. However, time is up....
